7930b8095c3bd8999a79214ff781dc6f6f6621e6
Alyona Berezorutska <av.golberg@gmail.com>
2026-02-01 09:11:33 +0200

    Add player movement animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float accel = 6f;
+
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    Animator animator;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    void Update()
+    {
+        Move();
+        FlipSprite();
+    }
+
+    void Move()
+    {
+        float targetX = moveInput.x * moveSpeed;
+        float newX = Mathf.MoveTowards(rb.linearVelocity.x, targetX, accel * Time.deltaTime);
+        if (Mathf.Abs(newX) < 0.05f) newX = 0f;
+
+        Vector2 playerVelocity = new Vector2(newX, rb.linearVelocity.y);
+        rb.linearVelocity = playerVelocity;
+
+        float speed01 = Mathf.Abs(rb.linearVelocity.x) / moveSpeed; // 0..1
+        animator.SetFloat("speed", speed01);
+    }
+
+    void FlipSprite()
+    {
+        bool isMoving = Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon;
+        if (isMoving)
+            transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocity.x), 1f);
+    }
+}
