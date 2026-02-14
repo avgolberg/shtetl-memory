d57ccbd5f64e2dfce487c22034aee084ebb9310c
Alyona Berezorutska <av.golberg@gmail.com>
2026-02-14 18:26:41 +0200

    Add popup on item pickup

--- Assets/Scripts/PlayerItemCollector.cs
+++ Assets/Scripts/PlayerItemCollector.cs
@@ -20,6 +20,7 @@ public class PlayerItemCollector : MonoBehaviour
 
                 if (itemAdded)
                 {
+                    item.PickUp();
                     Destroy(collision.gameObject);
                 }
             }
